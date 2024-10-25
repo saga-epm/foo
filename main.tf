fb34f557c5004905a7f84a4517f0ae6f48b288e5
sathish gajula <sathish.gajula@epm.net>
2024-10-25 10:12:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,4 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}  
-  subscription_id = "56d01180-6611-4eca-b2e4-31f57552288d"
-}
-
 variable "app_name" {}
-
-data "azurerm_resource_group" "example" {
-  name     = "Oliver"  
-}
